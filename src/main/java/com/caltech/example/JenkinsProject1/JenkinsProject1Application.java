ee1c711bae927f3192cb5caad90e916e83bc202e
Sanjay Raghuwanshi <raghu1c.sanjay@gmail.com>
2021-06-30 14:53:26 +0530

    first commit

---
+++ src/main/java/com/caltech/example/JenkinsProject1/JenkinsProject1Application.java
@@ -0,0 +1,13 @@
+package com.caltech.example.JenkinsProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/example/JenkinsProject1/JenkinsProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.example.JenkinsProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
